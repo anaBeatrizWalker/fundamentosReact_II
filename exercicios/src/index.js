5f3aa5cf90e4072c86951d36009c8ddf192e5cd0
Ana Beatriz <anabiawalker@gmail.com>
2022-01-21 17:41:03 -0300

    Comunicação Indireta entre Componentes

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import FamiliaWalker from './componentes/FamiliaWalker'
-import Familia from './componentes/Familia'
-import Membro from './componentes/Membro'
-import CompComFuncao from './componentes/CompComFuncao'
+import Pai from './componentes/Pai'
+
  
 ReactDOM.render(
     <div>
-        <FamiliaWalker/>
-
-        <Familia sobrenome='Conceição'>
-            <Membro nome='Cláudia'/>
-            <Membro nome='Severina'/>
-            <Membro nome='Júlia'/>
-        </Familia>
-
-        <CompComFuncao/>
+        <Pai/>
     </div>
     , document.getElementById('root')
 )
