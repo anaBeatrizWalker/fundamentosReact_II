4a0ea1389a6502afdef41e173e93e7f916eb48df
Ana Beatriz <anabiawalker@gmail.com>
2022-01-21 15:28:56 -0300

    Primeiro Componente

---
+++ exercicios/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Primeiro from './componentes/Primeiro'
+ 
+ReactDOM.render(
+    <Primeiro/>
+    , document.getElementById('root')
+)
